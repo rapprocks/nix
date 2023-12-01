5ea2768fcb7cefa73ad656eee1ac42dc818fa411
rapprocks <rapprocks@users.noreply.github.com>
2023-12-01 10:38:13 +0100

    added shell and nvim config

---
+++ home/nvim.nix
@@ -0,0 +1,8 @@
+{ config, ... }:
+{
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+  };
+}
---
+++ home/shell.nix
@@ -0,0 +1,40 @@
+{ config, pkgs, ... }:
+
+{
+
+  programs = {
+
+    zsh = {
+      enable = true;
+      dotDir = ".config/zsh";
+      enableAutosuggestions = true;
+      enableCompletion = true;
+      enableSyntaxHighlighting = true;
+      autocd = true;
+      shellAliases = {
+        rebuild = "sudo nixos-rebuild switch";
+        vi = "nvim";
+        l = "exa";
+        ip = "ip --color";
+        la = "exa -a";
+        ll = "exa -lah";
+        ls = "exa --color=auto";
+        cp = "rsync -ah --progress";
+        docs = "cd /mnt/nas/documents";
+        down = "cd /mnt/nas/downloads";
+        media = "cd /mnt/nas/media";
+        dot = "cd ~/dotfiles";
+        tree = "tree -C";
+        dev = "cd ~/Developer";
+      };
+      oh-my-zsh = {
+        enable = true;
+        plugins = [
+          "git"
+          "ssh-agent"
+        ];
+        theme = "robbyrussell";
+      };
+    };
+  };
+}
